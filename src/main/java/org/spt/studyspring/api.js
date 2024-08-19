550f15202b5ea67eea235853771bef4c4ad71b3b
anhaosama <1350194675@qq.com>
2024-08-19 17:20:28 +0800

    Commit 00c version0.0

--- src/main/java/org/spt/studyspring/api.js
+++ src/main/java/org/spt/studyspring/api.js
@@ -1,4 +1,12 @@
 function askVerifyCode(){
     get("127.0.0.1")
     .then(function(result){})
+    .catch(function(err){})
+
+}
+
+function  login(){
+    post("127.0.0.1")
+    .then(function(result){})
+    .catch(function(err){})
 }
